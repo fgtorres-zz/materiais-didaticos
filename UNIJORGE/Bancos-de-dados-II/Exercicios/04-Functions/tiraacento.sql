f231fbee80e8dd977a251c91c58800763e96371d
Felipe Torres <fgtorres18@gmail.com>
2021-04-01 08:59:43 -0300

    Create tiraacento.sql

---
+++ UNIJORGE/Bancos-de-dados-II/Exercicios/04-Functions/tiraacento.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION tiraacento(texto TEXT)
+RETURNS TEXT AS $$
+DECLARE resultado TEXT;
+BEGIN
+
+	-- Retirando os acentos do texto e deixando ele UPPER
+        SELECT  UPPER($1) INTO resultado;
+        SELECT  REPLACE(resultado, 'Ã', 'A') INTO resultado;
+        SELECT  REPLACE(resultado, 'É', 'E') INTO resultado;
+        SELECT  REPLACE(resultado, 'Ó', 'O') INTO resultado;
+        SELECT  REPLACE(resultado, 'Õ', 'O') INTO resultado;
+
+        RETURN resultado;
+
+END;
+$$  LANGUAGE plpgsql
+
+
+
+
