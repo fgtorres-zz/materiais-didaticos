35054d6524e2ab269c7ab202c9829dd95d1124ac
Felipe Torres <fgtorres18@gmail.com>
2021-04-29 07:44:31 -0300

    atualizasalario

---
+++ UNIJORGE/Bancos-de-dados-II/Exercicios/04-Functions/atualizasalario.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR REPLACE FUNCTION atualizasalario(codemp INT, piso INT, percentual INT)
+RETURNS INT AS $$
+DECLARE percentualparcial INT;
+DECLARE salarioparcial INT;
+BEGIN
+
+  
+    
+    RETURN percentualparcial;
+
+END;
+$$  LANGUAGE plpgsql
+
+
