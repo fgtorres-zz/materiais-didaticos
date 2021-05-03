81a963f8f72b6e6e85b763c5be8c8a43e0a5d3d5
Felipe Torres <fgtorres18@gmail.com>
2021-05-03 08:55:47 -0300

    add

---
+++ UNIJORGE/Bancos-de-dados-II/Exercicios/07-Triggers/cidade_trigger_b.sql
@@ -0,0 +1,16 @@
+﻿CREATE TRIGGER cidade_trigger_bef_ins
+BEFORE INSERT ON cidade
+FOR EACH ROW
+EXECUTE PROCEDURE cidade_log_func();
+
+CREATE OR REPLACE FUNCTION cidade_log_func()
+   RETURNS trigger AS $BODY$
+BEGIN
+   -- OLD / NEW 
+   new.nome = UPPER(new.nome);
+   RETURN new;
+   
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
